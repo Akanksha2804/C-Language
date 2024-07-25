ce4ad4dac3887e8c300e6e17ab3cd78774e50329
Akanksha2804 <shasakanksh752@gmail.com>
2024-07-25 21:20:16 +0530

    Add files via upload

---
+++ Akanksha.coding.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h
+#include<conio.h
+int main ()
+{
+	printf(Welcome);
+	printf(My Name is Akanksha Shah);
+	printf(I study Bvoc IT in Shani Bsiness School College Ahmedabad
+	);
+	printf()
+	
+}
