9b42da1e79540c7a5773478223eef5bd21bd791c
Akanksha2804 <shasakanksh752@gmail.com>
2024-08-18 18:26:50 +0530

    Update Akanksha.coding.cpp

--- Akanksha.coding.cpp
+++ Akanksha.coding.cpp
@@ -1,10 +1,10 @@
-#include<stdio.h
-#include<conio.h
+#include<stdio.h>
+#include<conio.h>
 int main ()
 {
-	printf(Welcome);
-	printf(My Name is Akanksha Shah);
-	printf(I study Bvoc IT in Shani Bsiness School College Ahmedabad
+	printf("Welcome");
+	printf("My Name is Akanksha Shah");
+	printf("I study Bvoc IT in Shanti Bsiness School College Ahmedabad"
 	);
 	printf()
 	
